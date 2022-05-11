b9e0a6c6b0d5c844b51e19b8282b50e3acd0ac56
Alvis <alvisfranks@gmail.com>
2022-05-12 02:05:29 +0300

    05 homework done.

--- Majasdarbs/05-majasdarbs/Object/cv.js
+++ Majasdarbs/05-majasdarbs/Object/cv.js
@@ -1,3 +1,20 @@
-const VardsUzvards = {
-    
+const AlvisFranks = {
+    userId: 420,
+    firstName: "Alvis",
+    lastName: "Franks",
+    age: 27,
+    email: "franks@e6.lv",
+    companyPosition: undefined,
+    education: "College",
+    phoneNumber: 22222222,
+    skills: {
+        frontendSkills:["HTML", "CSS", "Javascript", "Figma"],
+        backEndSkills:["Python3", "NodeJS"]
+    },
+    car:{
+        make: "Volvo",
+        model: 740,
+        engine: "B230ft",
+        turbo: true,   
+    },
 }
