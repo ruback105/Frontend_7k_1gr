c4bec0b5fccd77f60e4a025ce9b768e1a3055c41
Gvido Zarins <gvido.zarins@inbox.lv>
2022-05-14 12:24:34 +0300

    Homework 5 DONE

---
+++ Majasdarbs/05-majasdarbs/GvidoZarins/cv.js
@@ -0,0 +1,11 @@
+const GvidoZarins = {
+    name: "Gvido",
+    lastName: "Zarins",
+    phoneNumber: "+371 26860094",
+    email: "gvido.zarins@inbox.lv",
+    workExperience: ["SIA BLS", "SIA ALB", "Auce municiplaity police", "AS SEB banka"],
+    education: ["Auce secondary school", "BA SAchool of Business and Finance"],
+    skills: ["HTML5", "CSS", "JavaScript", "Bootstrap"],
+    hobbies: ["Motorcycles", "Nature", "Programming"],
+    languages: ["Latvian(native)", "English(working professional)", "Russian(basic)"]
+}
