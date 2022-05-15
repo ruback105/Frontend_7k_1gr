bb3d405c14e7e3cac433c15be3247957e4384076
Sergejs-Vasjkovs <s.vasjkovs@gmail.com>
2022-05-15 21:03:00 +0300

    Majas darbs #5

--- Majasdarbs/05-majasdarbs/Object/cv.js
+++ Majasdarbs/05-majasdarbs/Object/cv.js
@@ -1,3 +1,55 @@
 const VardsUzvards = {
-    
-}
+    name: "John Doe",
+    position: "Full stack developer",
+    education: {
+        College: {
+            name: "MSc in Computer Science",
+            years: "2015-2019",
+        },
+        University: {
+            name: "BSc Maths and Physics",
+            years: "2007 - 2010",
+        }
+    },
+    skills: {
+        frontend: {
+            1: "React/Redux/Angular",
+            2: "Javascript",
+            3: "Node.js",
+            4: "Webpack/Gulp/Grunt",
+            5: "HTML/CSS/SASS/LESS",
+        },
+        Backend: {
+            1: "Python/Django",
+            2: "Ruby/Rails",
+            3: "PHP",
+            4: "PostgresSQL/MySQL",
+            5: "MongoDB",
+        },
+        Others: {
+            1: "DevOps",
+            2: "Unit Testing",
+            3: "UX/Wireframing",
+            4: "Sketch/Balsamiq",
+            5: "Wordpress/Shopify",
+        }
+    },
+    experience: {
+        Google: {
+            Position: "Intern",
+            years: "2019",
+        },
+        TSystems: {
+            Position: "Junior Front-End developer",
+            years: "2019-2020",
+        },        
+        ITGroup: {
+            Position: "Middle WEB developer",
+            years: "2020-2022",
+        },
+        Spotify: {
+            Position: "Senior Fullstack developer",
+            years: "2022 - now",
+        }
+    }
+};
