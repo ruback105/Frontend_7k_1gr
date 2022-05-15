a89fa3b7fa5e60141c727a4e41131d08c132487e
Olga Drozdova <olga.drozdova@hotmail.com>
2022-05-15 18:01:06 +0300

    5 majas darbs

--- Majasdarbs/05-majasdarbs/Object/cv.js
+++ Majasdarbs/05-majasdarbs/Object/cv.js
@@ -1,3 +1,12 @@
 const VardsUzvards = {
+    name: "Olga",
+    surname: "Drozdova",
+    phone: "+37129279886",
+    email: "olga.drozdova@hotmail.com",
+    hobbies: {
+        reading,
+        walking_in_the_forest,
+    }
+    languages: ["Russian (native)", "English (working Professional)", "Latvian (working Professional)"]
     
 }
