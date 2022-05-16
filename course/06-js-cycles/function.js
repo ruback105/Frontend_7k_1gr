0f2a2da7b6ca9d8d213ebfd070657d001a6b6ffc
Arturs Pusko <arturs.pusko@gmail.com>
2022-05-16 20:47:18 +0300

    add second task

---
+++ course/06-js-cycles/function.js
@@ -0,0 +1,72 @@
+/* Set variable inside a function */
+
+// const x = 2;
+// const y = 3;
+
+// let result; // undefined
+
+// console.log(result);
+
+// const sum = (param1, param2) => {
+//   result = param1 + param2; // 5
+// };
+
+// sum(x, y);
+
+// console.log(result);
+
+/* Function with return */
+
+// const x = 4;
+// const y = 9;
+
+// const sum = (param1, param2) => {
+//   const result = `Result of sum is: ${param1 + param2}`;
+//   return result;
+// };
+
+// const result = sum(x, y); // 13
+
+// console.log(result);
+
+/* Try catch block */
+// const emails = [
+//   "  artuRs@dev.lv  ",
+//   " rnj3i@inbox.lv",
+//   "  tesT@gMail.com",
+//   0,
+//   "arturs@gmail.com",
+//   NaN,
+//   " elon@musk.coM",
+//   123,
+// ];
+
+// const parseEmail = (email) => {
+//   try {
+//     return email.trim().toLocaleLowerCase();
+//   } catch (e) {
+//     console.warn(e);
+//   }
+// };
+
+// const parsedEmails = [];
+
+// emails.forEach((email) => {
+//   const parsedEmail = parseEmail(email);
+
+//   if (parsedEmail) {
+//     parsedEmails.push(parsedEmail);
+//   }
+// });
+
+// console.log(parsedEmails);
+
+const array = [1, 2, 3, "test", 4, NaN, 5, false];
+
+const numberArray = [];
+
+// create funciton to validate if is number
+
+array.forEach((arrayElem) => {});
+
+console.log(numberArray); // [1,2,3,4,5]
