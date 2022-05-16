77471979e6edfc55ec08de99c1bd099b17ef5e01
STANISLAVS LOPATINSKIS <stanislavs.lopatinskis@gmail.com>
2022-05-16 13:46:47 +0300

    05-stanislavs-lopatinskis

--- Majasdarbs/05-majasdarbs/Object/cv.js
+++ Majasdarbs/05-majasdarbs/Object/cv.js
@@ -1,3 +1,21 @@
 const VardsUzvards = {
+    name: "Stanislavs"
+    surname: "Lopatinskis"
+    email: "stanislavs.lopatinskis@gmail.com"
+    education: {
+        "Politikas zinātnes maģistrs 2011-2014"
+        "Politikas zinātnes bakalaurs 2008-2011"
+    }
+    skills: {
+        "Adobe Lightroom"
+        "Adobe Photoshop"
+        "Google Optimize"
+    }
+    languages: {
+        "Latvian"
+        "Russian"
+        "English"
+    }
+    contacts: "+371 26167865"
     
 }
---
+++ Majasdarbs/05-majasdarbs/if-else/switch.js
@@ -0,0 +1,23 @@
+const getDay = (darbaDiena) => {
+switch (darbaDiena) {
+    case "Pirmdiena":
+        return 1;
+    case "Otrdiena":
+        return 2;
+    case "Trešdiena":
+        return 3;
+    case "Ceturtdiena":
+        return 4;
+    case "Piektdiena":
+        return 5; 
+    case "Sestdiena":
+        return 6; 
+    case "Svētdiena":
+        return 7;
+    default: 
+        return "Error";
+}
+};
+
+console.log(getDay ("Piektdiena"))
+
