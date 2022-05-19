e03ba2e03f43fffccc081c6dbc9d18e21df832e7
AnniLovc <lrvcom@inbox.lv>
2022-05-19 13:25:49 +0300

    05 majasdarbs

---
+++ Majasdarbs/05-majasdarbs/05-anna-lovcinovska/if-else.js
@@ -0,0 +1,30 @@
+const getDay = (darbaDiena)=>{  
+    switch (darbaDiena) {
+        case "Pirmdiena":
+            return 1;
+            break;
+        case "Otrdiena":
+            return 2;
+            break;
+        case "Trešdiena":
+            return 3;
+            break;
+        case "Ceturtdiena":
+            return 4;
+            break;
+        case "Piektdiena":
+            return 5;
+            break;
+        case "Sestdiena":
+            return 6;
+        case "Svētdiena":
+            return 7;
+            break;
+    
+        default:
+            return (Error)
+    
+        } 
+        
+    }
+    
---
+++ Majasdarbs/05-majasdarbs/05-anna-lovcinovska/object.js
@@ -0,0 +1,17 @@
+const VardsUzvards = {
+    name: "Anna",
+    surname: 'Lovcinovska',
+    email: "lovcinovska.anna@gmail.com",
+    contacts: "29795356",
+    skills: {
+       Frontend: ["HTML", "CSS", "JavaScript"],
+       Webdesign: ["Adobe lightroom", "Adobe photoshop"]
+    },
+    
+    languages: {
+        Latvian: "intermediate",
+        English: "intermediate",
+        Russian: "native"
+    }
+ 
+}
