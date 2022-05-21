f38b8d71e0a207d2164dc9aa5387b0c53835dbbc
Olegs-github <oleg-k@inbox.lv>
2022-05-21 15:59:17 +0300

    Homework 6 - comments applied (not for checking)

--- Majasdarbs/06-majasdarbs/OlegsKozlovs/app.js
+++ Majasdarbs/06-majasdarbs/OlegsKozlovs/app.js
@@ -1,21 +1,20 @@
 const characters = ["a", "b", "c", "d", "e", "g", "k", "m", "o", "q", "t", "z"];
 
 let arrayId;
+let promptMessage;
 
 let output = document.getElementById("main");
 
-const printValues = () => { output.innerText = characters[arrayId]};
+const printValues = (elem) => { elem.innerText = characters[arrayId]};
 
-const getValue = () => {
-    arrayId = prompt("Please provide character index:");
-    if (!isNaN(arrayId)) {
-        if (arrayId >= 0 && arrayId <= characters.length - 1) {
-            printValues();
-            return;
-        }
+const getValue = (message = null) => {
+    arrayId = prompt(message ?? `Please provide character index from 0 till ${characters.length - 1}:`);
+    if(arrayId >= 0 && arrayId <= characters.length - 1 && !isNaN(arrayId)) {
+        printValues(output);
+        return;
     }
-    alert("Invalid index has been provided! Please try again.");
-    getValue();
+    promptMessage = `Invalid index has been provided! Please try again from 0 till ${characters.length - 1}:`;
+    getValue(promptMessage);
 }
 
 getValue();
