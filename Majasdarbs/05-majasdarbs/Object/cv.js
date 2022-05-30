52433dcdf31d85b1d96c25ed02080013ecf0fc21
nauriscoding <na.kostjuks@gmail.com>
2022-05-30 16:04:41 +0300

    commit

--- Majasdarbs/05-majasdarbs/Object/cv.js
+++ Majasdarbs/05-majasdarbs/Object/cv.js
@@ -1,3 +1,19 @@
-const VardsUzvards = {
-    
-}
+const Person = {
+    name:'Nauris',
+    surname:'Kostjuks',
+    age:'32',
+    birth_place:'Latvia, Liepaja',
+    position:'Front end web developer',
+    }
+
+    let name = Person.name;
+    let surname = Person.surname;
+    let age = Person.age;
+    let birth_place = Person.birth_place;
+    let position = Person.position;
+
+    console.log(name)
+    console.log(surname)
+    console.log(age)
+    console.log(birth_place)
+    console.log(position)
