1b6a55c55bcd528a3afa1a92d6660789361fe136
Arturs Pusko <arturs.pusko@gmail.com>
2022-06-06 19:50:35 +0300

    add fetch task

---
+++ course/09-api/js/app.js
@@ -0,0 +1,15 @@
+// const fetchUser = async () => {
+//   const user = await fetch("https://randomuser.me/api/")
+//     .then((res) => res.json())
+//     .then((data) => data.results);
+
+//   console.log(user);
+// };
+
+// fetchUser();
+
+const fetchUsers = () => {
+  // https://jsonplaceholder.typicode.com/ - fetch users api
+  
+  //   console.log(users)
+};
