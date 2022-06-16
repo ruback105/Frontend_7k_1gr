0f7a9fc690d2438710fed363298886cd489fc48a
Ilze Mitre <ilzemitre@Ilzes-MacBook-Pro.local>
2022-06-16 23:55:29 +0300

    finished homework

--- Majasdarbs/05-majasdarbs/Object/cv.js
+++ Majasdarbs/05-majasdarbs/Object/cv.js
@@ -1,9 +1,104 @@
-const VardsUzvards = {
-    
-}let needTacos = true;
- 
-if (needTacos) {
-    console.log("Finding tacos");
-} else {
-    console.log("Keep on keeping on!");
+const cv = {
+    title: "Ilze Mitre | Portfolio",
+    header: {
+        nav: [
+            {
+                title: "General",
+                link: "#general"
+            },
+            {
+                title: "Work experience",
+                link: "#work-experience"
+            },
+            {
+                title: "Education",
+                link: "#education"
+            },
+            {
+                title: "Licenses & certifications",
+                link: "#licenses-certifications"
+            },
+            {
+                title: "Skills & Languages",
+                link: "#skills"
+            },
+            {
+                title: "Free time activities",
+                link: "#fta"
+            },
+            {
+                title: "Projects",
+                link: "#projects"
+            },
+            {
+                title: "Contact me",
+                link: "#contact-me"
+            }
+            
+        ]
+    },
+    main: {
+        general: {
+            img: {
+                src: "./IMG_ilze-mitre.jpeg",
+                alt: "Author in SF"
+            },
+            title: "Ilze Mitre",
+            bio: "Wanna be a Frontend Developer",
+            nationality: "Latvian",
+            location: "Riga, Latvia", 
+            phone: "+371 261 23 123",
+            email: "name.surname@gmail.com"
+        },
+        workExpierence1: {
+            position: "HR Manager",
+            company: "Wandoo Finance Group",
+            period: "Feb 2019 - Apr 2022",
+            duties: [
+                "Full recruitment cycle", "HR administration", "HRIS implementation"
+            ]
+        },
+        workExpierence2: {
+            poasition: "Office Manager/HR Specialist",
+            company: "Wandoo Finance Group",
+            period: "Feb 2016 - Feb 2019",
+            duties: [
+                "Office administration", "Recruitment", "Assist to CEO"
+            ]
+
+        },
+        education: {
+            university: "University of Latvia",
+            degreeOne: "Master of Science",
+            degreeTwo: "Bachelor of Science (BSc)"
+        },
+        licensesCertifications: [
+            "Codecademy", "Udemy", "Eazel"
+        ],
+        skills: [
+            'Project Management', 
+            'Project Planning',
+            'Human Resources'
+        ],
+        freeTime: [
+            'Travelling with family', 'Dog training', 'Dancing'
+        ],
+        contactMe: {
+            firstName: " ", 
+            lastName: " ",
+        }
+    },
+    footer: {
+        links: [
+            {
+                link:"https://github.com/IlzeiMilzei",
+                title: "GitHub"
+            },
+            {
+                link:"https://www.linkedin.com/in/ilze-mitre-b4631956/",
+                title: "Linkedin"
+            }
+        ]
+
+    }
 }
